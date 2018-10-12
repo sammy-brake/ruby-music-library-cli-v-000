8a952150715e95a5461a7feee1f3d3724b02c77b
Samantha brake <sammy.brake@gmail.com>
2018-10-12 00:22:24 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -49,7 +49,7 @@ end
   end 
  
   def list_artists 
-    Artist.all.sort {|a, b| a.name <=> b.name}.each_with_index {|artist, index| binding.pry puts "#{index +1}. #{artist.name}"}
+    Artist.all.sort {|a, b| a.name <=> b.name}.each_with_index {|artist, index|  puts "#{index +1}. #{artist.name}"}
   end 
   
   def list_genres
